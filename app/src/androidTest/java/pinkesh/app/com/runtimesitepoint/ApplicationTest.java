c8d65a986b2a9d42f478fdba22784a73a90e3f91
pavan <pavanmehta@collegebol.com>
2016-06-14 16:04:50 +0530

    Initial commit

---
+++ app/src/androidTest/java/pinkesh/app/com/runtimesitepoint/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pinkesh.app.com.runtimesitepoint;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
